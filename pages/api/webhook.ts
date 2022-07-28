cf49aa686b71250d0ceb39ae5a1eadeaea51e592
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-07-28 22:27:38 +0900

    update qpi

--- pages/api/webhook.ts
+++ pages/api/webhook.ts
@@ -2,9 +2,8 @@ import { NextApiHandler } from "next";
 
 const handler: NextApiHandler = async (req, res) => {
   try {
-    console.log(req.body);
-    // const handle = req.body.contents.handle;
-    // await res.unstable_revalidate(`/${handle}`);
+    const handle: string = req.body.handle;
+    await res.revalidate(`/${handle}`);
 
     return res.status(200).send({});
   } catch (err) {
