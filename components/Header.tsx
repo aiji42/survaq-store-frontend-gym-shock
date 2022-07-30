846c811abebebffe7677934a4c2bffadba4b1295
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-07-30 21:00:00 +0900

    update logos

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
-import logo from "../public/logo.jpg";
+import logo from "../public/logo_black.png";
 
 export const Header = () => {
   return (
     <header className="item_header">
       <div className="item_logo">
-        <Image src={logo} alt="SurvaQ Store" priority height={50} width={100} />
+        <Image src={logo} alt="Furez" priority />
       </div>
     </header>
   );
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -9,8 +9,8 @@ export default function Document() {
         <meta charSet="utf-8" />
         <meta name="format-detection" content="telephone=no" />
         <link rel="shortcut icon" href="/favicon.ico" />
-        <link rel="apple-touch-icon" href="/survaq_logo_180x180.png" />
-        <link rel="icon" type="image/png" href="/survaq_logo_192x192.png" />
+        <link rel="apple-touch-icon" href="/icon_180x180.png" />
+        <link rel="icon" type="image/png" href="/icon_192x192.png" />
         <meta name="robots" content="noindex,nofollow" />
       </Head>
       <body>
