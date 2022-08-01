d940d9ad2e1d1ff85876ba7f140af6c210cc8ef4
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-08-01 22:18:10 +0900

    update home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
 import type { GetServerSideProps, NextPage } from "next";
 
+const productSets: { handle: string; productId: number }[] = JSON.parse(
+  process.env.PRODUCT_HANDLES ?? "[]"
+);
+
 export const getServerSideProps: GetServerSideProps = async () => {
-  const handle = process.env.PRODUCT_HANDLES?.split(",")[0];
+  const handle = productSets[0].handle;
   if (handle)
     return {
       redirect: {
