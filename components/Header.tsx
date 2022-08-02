6f41fe1f2017d0a67ed6dd41697e8b241c5dc313
Aiji Uejima <uejima.aiji@a-tm.co.jp>
2022-08-02 12:56:14 +0900

    Add preload

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,11 @@
+import Head from "next/head";
+
 export const Header = () => {
   return (
     <header className="item_header">
+      <Head>
+        <link href="/logo_black.png" as="image" rel="preload" />
+      </Head>
       <div className="item_logo">
         <img src="/logo_black.png" alt="Furez" />
       </div>
